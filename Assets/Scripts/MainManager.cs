1299145a3d1cc92eda5daaa8c8a31c85ccb88a98
aufklaeranlage <asbronner@gmail.com>
2024-02-12 13:21:07 +0100

    user name input + highscore fields implementation
    -added the connection to use user input for names
    -added connections to show highscores and the names of the score holders in menu and in main

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,39 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
+using UnityEditor;
+using TMPro;
 
 public class MainManager : MonoBehaviour
 {
+    public static MainManager Instance;
+    
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
 
+    public static string playerName;
+    public Text NameText;
+
     public Text ScoreText;
     public GameObject GameOverText;
+
+    public TextMeshProUGUI MenuHighScoresText;
+    public static List<int> highScores = new List<int>();
+    public static List<string> highScoreNames = new List<string>();
     
     private bool m_Started = false;
     private int m_Points;
     
     private bool m_GameOver = false;
 
+    // Make this MainManager an Instance
+    private void Awake() {
+        if(Instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
     
     // Start is called before the first frame update
     void Start()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
-        }
+        MenuHighScores();
     }
 
     private void Update()
@@ -57,7 +65,33 @@ public class MainManager : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    void GetObjects()
+    {
+        Ball = GameObject.FindWithTag("Ball").GetComponent<Rigidbody>();
+        NameText = GameObject.FindWithTag("Name Text").GetComponent<Text>();
+        ScoreText = GameObject.FindWithTag("Score Text").GetComponent<Text>();
+        GameOverText = GameObject.FindWithTag("Game Over Text");
+    }
+
+    void CreateBricks()
+    {
+        const float step = 0.6f;
+        int perLine = Mathf.FloorToInt(4.0f / step);
+        
+        int[] pointCountArray = new [] {1,1,2,2,5,5};
+        for (int i = 0; i < LineCount; ++i)
+        {
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint);
             }
         }
     }
@@ -72,5 +106,42 @@ public class MainManager : MonoBehaviour
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(1).completed += (x) => {
+            GetObjects();
+            CreateBricks();
+        };
+        
+    }
+
+    void MenuHighScores()
+    {
+        MenuHighScoresText = GameObject.FindWithTag("Menu High Score Text").GetComponent<TextMeshProUGUI>();
+        for(int i = 0; i < highScoreNames.Count; i++) 
+        {
+            MenuHighScoresText.text += highScoreNames[i] + ": " + highScores[i] + "\n";
+        }
+    }
+
+    void MainHighScore()
+    {
+        NameText.text = "Best Score: " + highScoreNames[1] + ": " + highScores[1];
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void GetPlayerName(string input)
+    {
+        playerName = input;
     }
 }
